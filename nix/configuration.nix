b7be5172970b570f0bd716dd9f2ccf9c0a726490
fark1 <ffarkgh@gmail.com>
2026-07-31 00:20:08 +0200

    fixed jj mishap

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -148,7 +148,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [ # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    jj
+    jujutsu
     nh
     fastfetch
     git
