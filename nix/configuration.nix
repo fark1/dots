da53b0640a9a504ebdca41fd2621f028c4ce52ed
fark1 <ffarkgh@gmail.com>
2026-08-04 17:13:29 +0200

    new programs

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -161,6 +161,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [ # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
+    neovim
     jujutsu
     nh
     fastfetch
@@ -199,6 +200,8 @@
     go
     scdoc
     gcc
+    gnumake
+    fd
   ];
   
 
